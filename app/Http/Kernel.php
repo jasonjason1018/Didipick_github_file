2fa0a97858272a98c68a0290239f1d3d805176ad
jason <jasonjason1018@gmail.com>
2021-09-22 16:56:19 +0800

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,10 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+
+        'admin_session' => [
+            \App\Http\Middleware\Admin_session::class,
+        ],
     ];
 
     /**
@@ -62,5 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin_session' => \App\Http\Middleware\Admin_session::class,
     ];
 }
